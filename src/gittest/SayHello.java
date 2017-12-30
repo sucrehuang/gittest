31e27a0356ca666b9d3f84458de57c2589d3eaf3
sucre.huang <huanggx330@outlook.com>
2017-12-30 17:52:47 +0800

    this is by eclipse update

--- src/gittest/SayHello.java
+++ src/gittest/SayHello.java
@@ -4,6 +4,8 @@ public class SayHello {
 
 	public static void main(String[] args) {
 		int i =1;//初始值
+		String tt="遥远的东方，古老的边疆";
+		String test = "天使的翅膀";
 		System.out.println("test"+SdKeyEnum.TD_CHECK_RSP.getRemark());
 		
 	}
