74450e7465c2688f0814ee0664bf348467453beb
sucre.huang <huanggx330@outlook.com>
2017-12-30 17:43:09 +0800

    不是eclipsee修改

--- src/gittest/SayHello.java
+++ src/gittest/SayHello.java
@@ -4,6 +4,7 @@ public class SayHello {
 
 	public static void main(String[] args) {
 		int i =1;//初始值
+		String test = "天使的翅膀";
 		System.out.println("test"+SdKeyEnum.TD_CHECK_RSP.getRemark());
 		
 	}
