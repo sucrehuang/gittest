97021e65bd859a1ba9917ea1a907aa4dc2bef4b6
sucre.huang <huanggx330@outlook.com>
2017-12-30 21:10:49 +0800

    uneclipse

--- src/gittest/SayHello.java
+++ src/gittest/SayHello.java
@@ -3,10 +3,14 @@ package gittest;
 public class SayHello {
 
 	public static void main(String[] args) {
-		int i =1;//初始值
+		int i =12;//初始值
+
 		String tt="遥远的东方，古老的边疆31";
+
 		String test = "天使的翅膀";
+	
 		String test_1 = "等下一个天亮";
+	
 		System.out.println("test"+SdKeyEnum.TD_CHECK_RSP.getRemark());
 		
 	}
