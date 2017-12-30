8139c16f630499cfcc62c68958ac32dff3398fac
sucre.huang <huanggx330@outlook.com>
2017-12-30 22:01:07 +0800

    提交知道有冲突

--- src/gittest/SayHello.java
+++ src/gittest/SayHello.java
@@ -4,9 +4,10 @@ public class SayHello {
 
 	public static void main(String[] args) {
 		int i =16;//初始值
-		String tt="遥远的东方，古老的边疆31";
+		String tt="遥远的东方，古老的边疆，万里长城永接壤";
 		String test = "天使的翅膀";
 		String test_1 = "等下一个天亮";
 		System.out.println("test"+SdKeyEnum.TD_CHECK_RSP.getRemark());
+		
 	}
 }
