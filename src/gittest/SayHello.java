756a77d485adefd78587b2cbdc3ec29414a593be
sucre.huang <huanggx330@outlook.com>
2017-12-31 10:43:04 +0800

    主干修改

--- src/gittest/SayHello.java
+++ src/gittest/SayHello.java
@@ -5,7 +5,7 @@ public class SayHello {
 	public static void main(String[] args) {
 		int i =16;//初始值
 		String tt="遥远的东方，古老的边疆，万里长城永接壤";
-		String test = "天使的翅膀";
+		String test = "天使的翅膀11";
 		String test_1 = "等下一个天亮";
 		System.out.println("test"+SdKeyEnum.TD_CHECK_RSP.getRemark());
 	}
